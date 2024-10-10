82a0d8dd11ea7b96e52c835b47fde02642f78b78
Earvizu17 <edgar.arvizu@bsu.edu>
2024-10-09 20:45:47 -0400

    finished product

--- src/App.js
+++ src/App.js
@@ -1,68 +1,50 @@
-// App.js 
-
 import "./App.css";
 import React, { useState } from "react";
 import { connect } from "react-redux";
 
-const App = 
-({ todos, addTodo, removeTodo, toggleTodo }) => {
-    const [text, setText] = useState("");
+const App = ({ todos, addTodo, removeTodo, toggleTodo }) => {
+    const [input, setInput] = useState("");
 
     const handleAddTodo = () => {
-        if (text.trim() !== "") {
+        if (input.trim()) {
             addTodo({
-                id: new Date().getTime(),
-                text,
+                id: Date.now(),
+                text: input,
                 completed: false,
             });
-            setText("");
+            setInput("");
         }
     };
 
-    const handleRemoveTodo = (id) => {
-        removeTodo(id);
-    };
+    const handleRemoveTodo = (id) => removeTodo(id);
 
-    const handleToggleTodo = (id) => {
-        toggleTodo(id);
-    };
+    const handleToggleTodo = (id) => toggleTodo(id);
 
     return (
         <div id="app">
-            <div>
-                <h1>To-Do List</h1>
-                <input
-                    type="text"
-                    value={text}
-                    onChange={(e) => 
-                        setText(e.target.value)}
-                    placeholder="Enter a task..."
-                />
-                <button onClick={handleAddTodo}>
-                    Add
-                </button>
-                <ul>
-                    {todos.map((todo) => (
-                        <li
-                            className="todo"
-                            key={todo.id}
-                            style={{
-                                textDecoration: todo.completed ? 
-                                "line-through" : "none",
-                                color: todo.completed ? 
-                                "red" : "black",
-                            }}
-                            onClick={() => handleToggleTodo(todo.id)}
-                        >
-                            {todo.text}
-                            <button onClick=
-                                {() => handleRemoveTodo(todo.id)}>
-                                Remove
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <h1>To-Do List</h1>
+            <input
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                placeholder="Enter a task..."
+            />
+            <button onClick={handleAddTodo}>Add</button>
+            <ul>
+                {todos.map((todo) => (
+                    <li
+                        key={todo.id}
+                        style={{
+                            textDecoration: todo.completed ? "line-through" : "none",
+                            color: todo.completed ? "red" : "black",
+                        }}
+                        onClick={() => handleToggleTodo(todo.id)}
+                    >
+                        {todo.text}
+                        <button onClick={() => handleRemoveTodo(todo.id)}>Remove</button>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
@@ -72,22 +54,10 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    addTodo: (todo) => 
-        dispatch({ 
-            type: "ADD_TODO", 
-            payload: todo 
-        }),
-    removeTodo: (id) => 
-        dispatch({ 
-            type: "REMOVE_TODO", 
-            payload: id 
-        }),
-    toggleTodo: (id) => 
-        dispatch({ 
-            type: "TOGGLE_TODO", 
-            payload: id 
-        }),
+    addTodo: (todo) => dispatch({ type: "ADD_TODO", payload: todo }),
+    removeTodo: (id) => dispatch({ type: "REMOVE_TODO", payload: id }),
+    toggleTodo: (id) => dispatch({ type: "TOGGLE_TODO", payload: id }),
 });
 
-export default connect
-    (mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
